8c538b3221f9635a88cb93f501dd1aa1922ee292
Sanjay Tailor <st.sanjaytailor@gmail.com>
2017-01-22 15:41:07 -0500

    search results are links instead of seperate links

--- js/app.js
+++ js/app.js
@@ -17,21 +17,21 @@ $(document).ready(function () {
             success: function (data) {
 
                 for (var i = 0; i < 10; i++) {
-                    $(".search-result ul").append("<li style='font-size: 40px'>" + data[1][i] + "</li><br>");
-                    $(".search-result ul").append("<li style='font-size: 20px'>" + data[2][i] + "</li<br>");
-                    $(".search-result ul").append("<li style='font-size: 30px'><a href='" + data[3][i] + "' target='_blank'> Read more at Wikipedia </a></li><br><hr>");
+                    $(".search-result ul").append("<a href='" + data[3][i] + "' target='_blank'>" +
+                        "<li style='font-size: 40px'>" + data[1][i] + "</li><br>"
+                        +    "<li style='font-size: 20px'>" + data[2][i] + "</li<br>"
+                        +   "</a><hr>");
                 }
-                console.log(searchTerm);
+                // console.log(searchTerm);
             }
         });
     }
 
-    $("#search-input").on("keypress", function (e) {
-
-        if (event.which == 13) {
+    $("#search-input").on("keypress", function (event) {
+        if (event.which === 13) {
             searchTerm = $(this).val();
             $(this).val("");
-            $(".search-result ul").empty();
+            $(".search-result ul").empty(); //clear the ul out before loading with a new searchTerm... without this code, page will not load with new searchTerm
             getWikipages();
         }
     });
